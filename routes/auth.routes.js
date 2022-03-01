507b48d1687e2e4274e52eed496f4c4366253f5d
dorrrn <doan7tran@gmail.com>
2022-03-01 22:36:16 +0100

    initial setup with jwt

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const jwt = require("jsonwebtoken");
+
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 // ℹ️ Handles password encryption
 const bcrypt = require("bcrypt");
@@ -10,15 +13,11 @@ const saltRounds = 10;
 // Require the User model in order to interact with the database
 const User = require("../models/User.model");
 
-// Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
-const isLoggedOut = require("../middleware/isLoggedOut");
-const isLoggedIn = require("../middleware/isLoggedIn");
-
 router.get("/loggedin", (req, res) => {
   res.json(req.user);
 });
 
-router.post("/signup", isLoggedOut, (req, res) => {
+router.post("/signup", (req, res) => {
   const { username, password } = req.body;
 
   if (!username) {
@@ -65,7 +64,6 @@ router.post("/signup", isLoggedOut, (req, res) => {
       })
       .then((user) => {
         // Bind the user to the session object
-        req.session.user = user;
         res.status(201).json(user);
       })
       .catch((error) => {
@@ -83,7 +81,7 @@ router.post("/signup", isLoggedOut, (req, res) => {
   });
 });
 
-router.post("/login", isLoggedOut, (req, res, next) => {
+router.post("/login", (req, res, next) => {
   const { username, password } = req.body;
 
   if (!username) {
@@ -113,9 +111,18 @@ router.post("/login", isLoggedOut, (req, res, next) => {
         if (!isSamePassword) {
           return res.status(400).json({ errorMessage: "Wrong credentials." });
         }
-        req.session.user = user;
-        // req.session.user = user._id; // ! better and safer but in this case we saving the entire user object
-        return res.json(user);
+
+        const payload = {
+          _id: user._id,
+          username: user.username,
+        };
+
+        const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
+          algorithm: "HS256",
+          expiresIn: "6h",
+        });
+
+        return res.json({ authToken: authToken });
       });
     })
 
@@ -127,13 +134,8 @@ router.post("/login", isLoggedOut, (req, res, next) => {
     });
 });
 
-router.get("/logout", isLoggedIn, (req, res) => {
-  req.session.destroy((err) => {
-    if (err) {
-      return res.status(500).json({ errorMessage: err.message });
-    }
-    res.json({ message: "Done" });
-  });
+router.get("/verify", isAuthenticated, (req, res, next) => {
+  res.json(req.payload);
 });
 
 module.exports = router;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,18 @@
 const router = require("express").Router();
 const authRoutes = require("./auth.routes");
+const projectRoutes = require("./project.routes");
+const taskRoutes = require("./task.routes");
+
+const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.json("All good in here");
 });
 
+/* All routes are mounting on /api */
 router.use("/auth", authRoutes);
+router.use("/projects", isAuthenticated, projectRoutes);
+router.use("/tasks", isAuthenticated, taskRoutes);
 
 module.exports = router;
