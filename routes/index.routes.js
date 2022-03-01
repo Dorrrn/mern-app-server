ac2928b2459140ba4ff2e1bb6bde08998715cd57
dorrrn <doan7tran@gmail.com>
2022-03-01 23:01:08 +0100

    connecting to client, mongoDB, postman -checked

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const authRoutes = require("./auth.routes");
-const projectRoutes = require("./project.routes");
-const taskRoutes = require("./task.routes");
+
 
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
@@ -12,7 +11,6 @@ router.get("/", (req, res, next) => {
 
 /* All routes are mounting on /api */
 router.use("/auth", authRoutes);
-router.use("/projects", isAuthenticated, projectRoutes);
-router.use("/tasks", isAuthenticated, taskRoutes);
+
 
 module.exports = router;
